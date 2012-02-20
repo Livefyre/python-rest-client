8e68212e159e6d7e69de9acf05433f7381ef71f3
Nino Walker <nino.walker@gmail.com>
2012-02-20 14:30:53 -0800

    Added a decent setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python_rest_client',
+      version='0.3.1',
+      description='Python Distribution Utilities',
+      author='',
+      author_email='',
+      url='https://github.com/Livefyre/python-rest-client',
+      package_dir={'': 'src'},
+      packages=['']
+     )
